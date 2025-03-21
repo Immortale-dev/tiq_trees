60495d58eace01545330448ebd83496d020ac3ff
Immortale <immortale.dev@gmail.com>
2025-03-21 15:40:07 +0000

    RopeTree added

---
+++ src/tq_rope_tree.h
@@ -0,0 +1,54 @@
+#ifndef TQ_ENGINE_DETAIL_ROPE_H_
+#define TQ_ENGINE_DETAIL_ROPE_H_
+
+#include <vector>
+#include <cstdint>
+#include <type_traits>
+
+#include "tq_tree.h"
+#include "tq_count_tree.h"
+
+namespace tiq::engine::detail {
+	template<typename T, typename Container = std::vector<T>>
+	class RopeNode : public tiq::tree::CountNode<Container> {
+		template<typename _N, typename _A> friend class RopeTree;
+
+		public:
+			using value_list_t = Container;
+			using value_t = T;
+
+			inline uint16_t size() const { return size_; }
+
+		protected:
+			uint16_t size_ = 0;
+	};
+
+	template<typename N, typename A = std::allocator<N>>
+	class RopeTree : public tiq::tree::CountTree<N> {
+		public:
+			using internal_node_ptr_t = tiq::tree::InternalNode*;
+			using node_ptr_t = N*;
+			using node_list_t = std::vector<node_ptr_t>;
+			using content_list_t = typename N::value_list_t;
+			using value_list_iterator = typename content_list_t::const_iterator;
+
+			explicit RopeTree(uint16_t value_size);
+
+		protected:
+			internal_node_ptr_t insert_(internal_node_ptr_t node) override;
+			void calc_count(internal_node_ptr_t x) override;
+			size_t size_() const override;
+
+			virtual void calc_size(internal_node_ptr_t node) const;
+			virtual value_list_iterator find_offset_item(content_list_t& container, value_list_iterator b, int16_t offset) const;
+			virtual void relax(node_ptr_t begin, node_ptr_t end);
+
+			uint16_t value_size_;
+			uint16_t value_min_size_;
+			uint16_t value_max_size_;
+	};
+}
+
+#include "tq_rope_tree.hpp"
+
+#endif // TQ_ENGINE_DETAIL_ROPE_H_
---
+++ src/tq_rope_tree.hpp
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <iterator>
+
+template<typename N, typename A>
+tiq::engine::detail::RopeTree<N,A>::RopeTree(uint16_t value_size) : value_size_(value_size), value_min_size_(value_size/2), value_max_size_(value_size*2) {}
+
+template<typename N, typename A>
+typename tiq::engine::detail::RopeTree<N,A>::internal_node_ptr_t tiq::engine::detail::RopeTree<N,A>::insert_(internal_node_ptr_t node)
+{
+	calc_size(node);
+	tiq::tree::CountTree<N,A>::insert_(node);
+	auto n = this->to_public_node(node);
+	if (n->size() > value_max_size_ || (n->size() < value_min_size_ && this->size() > n->size())) {
+		relax(n, this->find_next(n));
+	}
+	return node;
+}
+
+template<typename N, typename A>
+void tiq::engine::detail::RopeTree<N,A>::calc_count(internal_node_ptr_t x)
+{
+	if (x->is_end()) return;
+	node_ptr_t node = this->to_public_node(x);
+	node->count_ = this->left(node)->count() + this->right(node)->count() + node->size();
+}
+
+template<typename N, typename A>
+size_t tiq::engine::detail::RopeTree<N,A>::size_() const
+{
+	return this->root()->count();
+}
+
+template<typename N, typename A>
+void tiq::engine::detail::RopeTree<N,A>::calc_size(internal_node_ptr_t node) const
+{
+	node_ptr_t n = this->to_public_node(node);
+	if (!n->data_) return;
+	n->size_ = n->data_->size();
+}
+
+template<typename N, typename A>
+typename tiq::engine::detail::RopeTree<N,A>::value_list_iterator tiq::engine::detail::RopeTree<N,A>::find_offset_item([[maybe_unused]] content_list_t& container, value_list_iterator b, int16_t offset) const
+{
+	if (offset > 0) {
+		if (std::distance(b, container.cend()) <= offset) return container.end();
+		return std::next(b, offset);
+	}
+	if (std::distance(container.cbegin(), b) < -offset) return std::prev(container.begin());
+	return std::prev(b, -offset);
+}
+
+template<typename N, typename A>
+void tiq::engine::detail::RopeTree<N,A>::relax(node_ptr_t begin, node_ptr_t end)
+{
+	auto n = begin;
+	size_t total_size = 0;
+	while (n != end) {
+		total_size += n->size();
+		n = this->find_next(n);
+	}
+	if (total_size < value_min_size_) {
+		if (!end->is_end()) {
+			end = this->find_next(end);
+		} else if(this->begin() != begin) {
+			begin = this->find_prev(begin);
+		}
+	}
+
+	node_list_t nodes;
+	n = begin;
+	total_size = 0;
+	while (n != end) {
+		nodes.push_back(n);
+		total_size += n->size();
+		n = this->find_next(n);
+	}
+	content_list_t content;
+	for (auto n : nodes) {
+		content.insert(content.end(), std::make_move_iterator(n->data().begin()), std::make_move_iterator(n->data().end()));
+	}
+
+	size_t rest = total_size % value_size_;
+	size_t cnt = total_size ? ((total_size - 1)/value_size_ + 1) : 0;
+	bool take_rest = (rest > 0 && rest < value_min_size_);
+	if (take_rest > 0 && cnt > 1) {
+		cnt--;
+	}
+
+	if (nodes.size() > cnt) {
+		// remove nodes that are going to be empty
+		for (size_t j=cnt;j<nodes.size();j++) {
+			this->erase(nodes[j]);
+		}
+		nodes.resize(cnt);
+	}
+
+	size_t i=0;
+	value_list_iterator b,e = content.begin();
+	for (;i<std::min(cnt,nodes.size());i++) {
+		size_t take = value_size_ + (take_rest ? rest : 0);
+		take_rest = false;
+		b = e;
+		e = find_offset_item(content, e, take);
+		this->insert(nodes[i], content_list_t(std::make_move_iterator(b), std::make_move_iterator(e)));
+	}
+
+	if (i == cnt) return;
+
+	n = this->before(end);
+	for (;i<cnt;i++) {
+		b = e;
+		e = find_offset_item(content, e, value_size_);
+		this->insert(n, content_list_t(b, e));
+		n = this->after(n);
+	}
+}
